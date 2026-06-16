72bb081966ccec8d9f2e29a3748848974d338ab3
yann-blkk <yan.lucas.yjj@gmail.com>
2026-06-16 11:01:37 -0300

    Add files via upload

--- temp/src/Login/script.js
+++ temp/src/Login/script.js
@@ -3,6 +3,15 @@ function login(){
     var nome = $("#nome").val()
     var senha = $("#senha").val()
 
-    
+    if(nome && senha && nome === "admin" && senha === "123456"){
+        const user = {
+            name: nome, 
+            dataEntrada: new Date(),
+            id: Math.floor(Math.random() * 100000)
+        } 
+        localStorage.setItem("usuario", JSON.stringify(user))
+        window.location.href = "../Loja"
+
+    }
 
 }
