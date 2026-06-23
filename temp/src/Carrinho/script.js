aff7631a2bab7b1bc75823140485b83d95e3f4dd
yann-blkk <yan.lucas.yjj@gmail.com>
2026-06-23 08:33:30 -0300

    Add files via upload

--- temp/src/Carrinho/script.js
+++ temp/src/Carrinho/script.js
@@ -27,7 +27,42 @@ $(document).ready(function(){
         localStorage.setItem("carrinho", JSON.stringify(carrinho))
         exibirCarrinho()
     }
-    
+    exibirCarrinho()  
 })
+function gerar(){
+    const listElement = document.getElementById("lista")
+    const totalElement = document.getElementById("total")
+    const listaClone = listElement.cloneNode(true)
+    $(listaClone).find("button").remove()
+    const listaHtml = listaClone.innerHTML
+    const totalHtml = totalElement.innerHTML
+    const conteudoHTML =`
+    <html>
+            <head>
+                <meta charset="UTF-8">
+            </head>
+        <body>
+            <h1>PEDIDO CONFIRMADO</h1>
+            <h3>Agradecemos sua compra e sua preferência</h3>
+            <br>
+            ${listaHtml}
+            <br>
+            <br>
+            ${totalHtml}
+        </body>
+    </html>
+    `
+    const blob = new Blob([conteudoHTML], {type: "application/msword"})
+    const link = document.createElement("a")
+
+    link.href = URL.createObjectURL(blob)
+    link.download = "pedido.doc"
+    link.click ()
+    document.getElementById("pedido").style.display = "block"
+}
+
+function successClose(){
+    document.getElementById("pedido").style.display = "none"
+}
 
 
